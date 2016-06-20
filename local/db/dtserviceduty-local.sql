3982fb0e75ce22b14bc0f3f0a91b7873e6163e96
joan.tolos <joan.tolos@thomsonreuters.com>
2016-06-20 18:24:41 +0200

    New insert for user subscribe duty

--- local/db/dtserviceduty-local.sql
+++ local/db/dtserviceduty-local.sql
@@ -1,16 +1,26 @@
 --GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' IDENTIFIED BY 'password' WITH GRANT OPTION;
 --FLUSH PRIVILEGES;
-drop table dt_duty cascade;
-drop table dt_user cascade;
+DROP TABLE dt_user_subscribe_duty;
+DROP TABLE dt_duty;
+DROP TABLE dt_user;
 
-CREATE TABLE dt_duty (
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL DEFAULT '',
-	PRIMARY KEY (id)
+CREATE TABLE `dt_user_subscribe_duty` (
+  `user_id` mediumint(9) NOT NULL,
+  `duty_id` mediumint(9) NOT NULL,
+  PRIMARY KEY (`user_id`,`duty_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE dt_user (
-	id MEDIUMINT NOT NULL AUTO_INCREMENT,
-    name varchar(100) NOT NULL DEFAULT '',
-    PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `dt_duty` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `dt_user` (
+  `id` mediumint(9) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL DEFAULT '',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+insert into dt_user values (1, 'Joan');
+commit;
